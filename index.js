66b269b78a04814727d82ebd6a5fbfe55162be4c
Lara <laraellenh@gmail.com>
2022-01-09 18:49:32 -0500

    commit tree 8fcef714172bd314ed46b3b8346f258c198c0c0b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = "not empty"
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!"
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting = `Welcome, ${currentUser[0]}!`
